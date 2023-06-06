18a2f2d8135f5883d15b6093bcdc70fe14c7c2cf
Fabricio <fabriciojosemacedo@gmail.com>
2023-06-06 14:53:41 -0300

    Implementando as condicionais

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def calculadora(a, b):
 	k = input('Deseja fazer qual operacao? ').upper()
-
+	if k == 'SOMA':
+		print(a + b)
+	if k == 'SUBTRACAO':
+		print(a - b)
+	if k == 'MULTIPLICACAO':
+		print(a * b)
+	if k == 'DIVISAO':
+		print(a / b)
+	
